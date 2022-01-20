82df00d81036c889c04be31e25ee65747eff4e48
37544 <losyukov@gmail.com>
2022-01-20 14:24:26 +0300

    Добавлен Lesson2

---
+++ Lesson2/src/main/java/Task2.java
@@ -0,0 +1,71 @@
+public class Task2 {
+
+    public class HomeWork {
+        public static void main(String[] args) {
+            //вывод на консоль
+            printLiterals();
+            //Некоторые тесты для проверки задач.
+            System.out.println(sum(100, 200));
+            System.out.println(sum(Integer.MAX_VALUE, Integer.MAX_VALUE));
+            System.out.println(max(56, 349));
+            System.out.println(calculateHypotenuse(3, 4));
+        }
+
+        private static void printLiterals() {
+//        вывести на консоль литералы следующих видов:
+//        логический
+//        строковый
+//        символьный
+//        целочисленный 2-й
+//        целочисленный 8-й
+//        целочисленный 10-й
+//        целочисленный 16-й
+//        литерал типа float
+//        литерал типа double.
+        }
+
+        /**
+         * 1. Метод должен вернуть сумму двух чисел a и b
+         * 2. Дополнительно: сделать проверку если сумма a и b больше чем максимальное значение int то вернуть -1
+         **/
+        public static int sum(int a, int b) {
+            return 0;
+        }
+
+        /**
+         * Метод должен вернуть максимальное значение из двух чисел
+         *
+         * <p>
+         * Example1:
+         * a = 4,
+         * b = 5
+         * <p>
+         * Метод должен вернуть 5
+         * Example2:
+         * a = 10,
+         * b = 10
+         * <p>
+         * Метод должен вернуть 10
+         */
+        public static int max(int a, int b) {
+            return 0;
+        }
+
+        /**
+         * Используя теорему Пифагора, вычислите значение гипотенузы. Квадрат гипотенузы = сумме квадратов катетов
+         * <p>
+         * Example1:
+         * 3
+         * 4
+         * return 5
+         * <p>
+         * Example2:
+         * 12
+         * 16
+         * return 20
+         */
+        public static double calculateHypotenuse(int a, int b) {
+            return 0;
+        }
+    }
+}
