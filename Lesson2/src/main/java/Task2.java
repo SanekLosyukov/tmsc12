93d4e3de4bf673e7bceccf7d0459d5f89fd8838a
37544 <losyukov@gmail.com>
2022-01-20 20:52:12 +0300

    Добавлен class Task3

--- Lesson2/src/main/java/Task2.java
+++ Lesson2/src/main/java/Task2.java
@@ -1,80 +1,84 @@
 public class Task2 {
+    public static void main(String[] args) {
 
-    public class HomeWork {
-        public void main(String[] args) {
-            //вывод на консоль
-            printLiterals();
-            //Некоторые тесты для проверки задач.
-            System.out.println(sum(100, 200));
-            System.out.println(sum(Integer.MAX_VALUE, Integer.MAX_VALUE));
-            System.out.println(max(56, 349));
-            System.out.println(calculateHypotenuse(3, 4));
-        }
+        //вывод на консоль
+        printLiterals();
+        //Некоторые тесты для проверки задач.
+/*        System.out.println(sum(100, 200));
+        System.out.println(sum(Integer.MAX_VALUE, Integer.MAX_VALUE));
+        System.out.println(max(56, 349));
+        System.out.println(calculateHypotenuse(3, 4));
+        */
 
-        private void printLiterals() {
-
-            boolean a = true;    //        логический
-            String b = "String";    //        строковый
-            char c = 'b';           //        символьный
-            int d = 0b1101;         //        целочисленный 2-й
-            int e = 010;            //        целочисленный 8-й
-            int f = 514;            //        целочисленный 10-й
-            int g = 0x6F;            //        целочисленный 16-й
-            float h = 1.2f;         //        литерал типа float
-            double i = 1.2;         //        литерал типа double
-            System.out.println(a);
-            System.out.println(b);
-            System.out.println(c);
-            System.out.println(d);
-            System.out.println(e);
-            System.out.println(f);
-            System.out.println(g);
-            System.out.println(h);
-            System.out.println(i);
-        }
-
-        /**
-         * 1. Метод должен вернуть сумму двух чисел a и b
-         * 2. Дополнительно: сделать проверку если сумма a и b больше чем максимальное значение int то вернуть -1
-         **/
-        public int sum(int a, int b) {
-            return 0;
-        }
-
-        /**
-         * Метод должен вернуть максимальное значение из двух чисел
-         *
-         * <p>
-         * Example1:
-         * a = 4,
-         * b = 5
-         * <p>
-         * Метод должен вернуть 5
-         * Example2:
-         * a = 10,
-         * b = 10
-         * <p>
-         * Метод должен вернуть 10
-         */
-        public int max(int a, int b) {
-            return 0;
-        }
-
-        /**
-         * Используя теорему Пифагора, вычислите значение гипотенузы. Квадрат гипотенузы = сумме квадратов катетов
-         * <p>
-         * Example1:
-         * 3
-         * 4
-         * return 5
-         * <p>
-         * Example2:
-         * 12
-         * 16
-         * return 20
-         */
-        public double calculateHypotenuse(int a, int b) {
-            return 0;
-        }
     }
+
+    private static void printLiterals() {
+
+        boolean a = true;    //        логический
+        String b = "String";    //        строковый
+        char c = 'b';           //        символьный
+        int d = 0b1101;         //        целочисленный 2-й
+        int e = 010;            //        целочисленный 8-й
+        int f = 514;            //        целочисленный 10-й
+        int g = 0x6F;            //        целочисленный 16-й
+        float h = 1.2f;         //        литерал типа float
+        double i = 1.2;         //        литерал типа double
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+    }
+
+    /**
+     * 1. Метод должен вернуть сумму двух чисел a и b
+     * 2. Дополнительно: сделать проверку если сумма a и b больше чем максимальное значение int то вернуть -1
+     **/
+    //public static sum(int a, int b) {
+    //    return 0;
+    // }
+
+    /**
+     * Метод должен вернуть максимальное значение из двух чисел
+     *
+     * <p>
+     * Example1:
+     * a = 4,
+     * b = 5
+     * <p>
+     * Метод должен вернуть 5
+     * Example2:
+     * a = 10,
+     * b = 10
+     * <p>
+     * Метод должен вернуть 10
+     */
+/*    public int max(int a, int b) {
+        return 0;
+    }
+*/
+    /**
+     * Используя теорему Пифагора, вычислите значение гипотенузы. Квадрат гипотенузы = сумме квадратов катетов
+     * <p>
+     * Example1:
+     * 3
+     * 4
+     * return 5
+     * <p>
+     * Example2:
+     * 12
+     * 16
+     * return 20
+     */
+/*    public double calculateHypotenuse(int a, int b) {
+        return 0;
+*/
+
 }
+
+
+
