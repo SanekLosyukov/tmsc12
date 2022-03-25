98af0eac51693873415dca8d1b4d5e4482614401
SanekLosyukov <losyukov@gmail.com>
2022-03-25 13:42:29 +0300

    ДЗ 14 исправление ошибок и доделал 4 задачу

--- Lesson14/src/main/java/files/Task3.java
+++ Lesson14/src/main/java/files/Task3.java
@@ -1,58 +1,41 @@
 package files;
 
-import org.apache.commons.io.FileUtils;
+import files.task2.TextFormatter;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Task3 {
     public static final String RESOURCE_DIR = "Lesson14/src/main/resources/";
     public static final String INPUT_FILE = RESOURCE_DIR + "input3.txt";
-    public static final String BLACK_LIST = RESOURCE_DIR + "blacklist.txt";
+    public static final String BLACKLIST = RESOURCE_DIR + "blacklist.txt";
     public static final String OUTPUT_FILE = RESOURCE_DIR + "output3.txt";
 
-    public static void main(String[] args) {
-        try {
-            List<String> stringList = FileUtils.readLines(new File(INPUT_FILE), "UTF-8");
-            List<String> stringBlackList = FileUtils.readLines(new File(BLACK_LIST), "UTF-8");
-            System.out.println(stringBlackList);
-            System.out.println(Arrays.toString(stringList.toString().toLowerCase().split("\\.")));
+    private static final String BLACKLIST_SEPARATOR = "[,\\s]+";
+    private static final String TEXT = "Lesson14/src/main/java/homework/result3/text.txt";
 
-            Pattern pattern = Pattern.compile(stringBlackList.toString());
-            Matcher matcher = pattern.matcher(stringList.toString());
-            while (matcher.find()) {
-                System.out.println(matcher.group());
-            }
-//            String replaceSymbols = stringList.toString()
-//                    .replaceAll("\\[", "")
-//                    .replaceAll("\\]", "");
-//            String[] string = replaceSymbols.split("\\.");   // сложил в массив предложения по отдельности
-//            String[] words = replaceSymbols.
-//                    replaceAll("\\.", "")
-//                    .split("\\s");                           // массив слов
-//            String[] stringBlack = stringBlackList.toString()
-//                    .replaceAll("\\[", "")
-//                    .replaceAll(",", "")
-//                    .replaceAll("\\]", "")
-//                    .split("\\s");                          // массив слов из блэклиста
-//            System.out.println(Arrays.toString(string));
-//            System.out.println(Arrays.toString(words));
-//            System.out.println(Arrays.toString(stringBlack));
+    public static void main(String[] args) throws IOException {
 
-//            for (int i = 0; i < words.length; i++) {
-//                if (words[i].equalsIgnoreCase(stringBlack.toString())) {
-//                    System.out.println("В тексте есть слова из blacklist");
-//                } else {
-//                    System.out.println("В тексте нет слов из blacklist");
-//                    break;
-//                }
-//            }
-        } catch (IOException exception) {
-            System.out.println("Unexpected error" + exception);
+        String text = Files.readString(Path.of(TEXT));
+        String[] blacklist = Files.readString(Path.of(BLACKLIST)).split(BLACKLIST_SEPARATOR);
+
+        if (!TextFormatter.hasBlacklistWordInString(text, blacklist)) {
+            System.out.println("В тексте не встечаются слова из чёрного списка.");
+            return;
+        } else {
+            System.out.println("В тексте обнаружены слова из чёрного списка." +
+                    "\nПредложения, подлежащие исправлению:");
         }
+        List<String> sentences = TextFormatter.getSentencesFromText(text);
+        int sentencesWithBLWords = 0;
+        for (String sentence : sentences) {
+            if (TextFormatter.hasBlacklistWordInString(sentence, blacklist)) {
+                System.out.println(sentence);
+                sentencesWithBLWords++;
+            }
+        }
+        System.out.println("Всего предложений, подлежащих исправлению: " + sentencesWithBLWords);
     }
 }
