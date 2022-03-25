361a7fef6467606c5a86a20bd51e1a5e64340f02
SanekLosyukov <losyukov@gmail.com>
2022-03-25 13:11:21 +0300

    ДЗ 17-18

---
+++ Lesson17.18/src/main/java/Book.java
@@ -0,0 +1,14 @@
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Book {
+    private String author; //Автор
+    private String name;    //Название
+    private Integer issueYear; //Год издания
+}
