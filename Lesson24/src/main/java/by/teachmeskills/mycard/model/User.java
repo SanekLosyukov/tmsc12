4c29762922d0424432e9038a9d0854b30d7f632a
SanekLosyukov <losyukov@gmail.com>
2022-03-30 22:09:53 +0300

    ДЗ №3. 1 задание (без дополнительного).

---
+++ Lesson24/src/main/java/by/teachmeskills/mycard/model/User.java
@@ -0,0 +1,13 @@
+package by.teachmeskills.mycard.model;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class User {
+    private String name;
+    private String password;
+}
